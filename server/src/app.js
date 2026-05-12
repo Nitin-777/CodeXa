620085f493fbf2aa86cdea250967a5d23272ab0c
Nitin <nitinsharmaop828@gmail.com>
2026-05-12 19:22:38 +0530

    app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,14 @@
 const express= require('express')
+import authRouter from "./routes/auth.routes.js";
 
-const app=express();
+
+const app = express();
+
+app.use(express.json());
+
+// Initialize Auth Router
+app.use("/auth", authRouter);
+
 
 module.exports=app;
 
